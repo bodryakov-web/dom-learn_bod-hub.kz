b58fc6dd883f1403f5848a81ca4735d1b98a66e2
Bodryakov.web <bodryakov.web@gmail.com>
2025-08-17 20:02:14 +0300

    .

--- index.php
+++ index.php
@@ -358,6 +358,7 @@ function render_header(string $title, bool $with_topbar = true): void {
         echo '<header class="topbar">';
         echo '<div class="container bar">';
         echo '<a class="brand" href="' . asset('/') . '">DOMLearn</a>';
+        echo '<div class="header-title">Искусство управления веб-страницей (Современный JavaScript DOM/BOM)</div>';
         echo '<div class="spacer"></div>';
         echo '<button id="themeToggle" class="icon-btn" title="Переключить тему">🌓</button>';
         echo '</div>';
